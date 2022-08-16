94f6c6b9801b72c13ea1451e1847f51842b2fcce
yang <young_yang95@163.com>
2022-08-16 10:53:18 +0800

    cell表格

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,142 @@
 <template>
   <div id="app">
-    <HelloWorld />
+    <MyForm :tableData="tableData" :titleData="titleData"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyForm from "./components/MyForm.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    MyForm,
+  },
+  data() {
+    return {
+      titleData: [
+        {
+          prop: "id",
+          label: "序号",
+          width: "150",
+        },
+        {
+          prop: "date",
+          label: "日期",
+          width: "150",
+        },
+        {
+          prop: "name",
+          label: "姓名",
+          width: "150",
+        },
+        {
+          prop: "province",
+          label: "省份",
+          width: "150",
+        },
+        {
+          prop: "city",
+          label: "市区",
+          width: "150",
+        },
+        {
+          prop: "address",
+          label: "地址",
+          width: "150",
+        },
+        {
+          prop: "zip",
+          label: "邮编",
+          width: "150",
+        },
+      ],
+      tableData: [
+        {
+          id:'1',
+          date: "2016-05-02",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333,
+        },{
+          id:'2',
+          date: "2016-05-02",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333,
+        },
+        {
+          id:'3',
+          date: "2016-05-02",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333,
+        },
+        {
+          id:'4',
+          date: "2016-05-02",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333,
+        },
+        {
+          id:'5',
+          date: "2016-05-02",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333,
+        },
+        {
+          id:'6',
+          date: "2016-05-02",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333,
+        },
+        {
+          id:'7',
+          date: "2016-05-02",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333,
+        },
+        {
+          id:'8',
+          date: "2016-05-02",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333,
+        },
+        {
+          id:'9',
+          date: "2016-05-02",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333,
+        }
+      ],
+     
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
---
+++ src/components/MyForm.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-table :data="tableData" border style="width: 100%">
+    <el-table-column
+      fixed
+      :prop="item.prop"
+      :label="item.label"
+      :width="item.width"
+      v-for="(item, index) in titleData"
+      :key="index"
+    >
+    </el-table-column>
+    <!--<el-table-column prop="name" label="姓名" width="120"> </el-table-column>
+        <el-table-column prop="province" label="省份" width="120"></el-table-column>
+        <el-table-column prop="city" label="市区" width="120"> </el-table-column>
+        <el-table-column prop="address" label="地址" width="300"> </el-table-column>
+        <el-table-column prop="zip" label="邮编" width="120"> </el-table-column> -->
+
+    <el-table-column fixed="right" label="操作">
+      <template slot-scope="scope">
+        <el-button @click="handleClick(scope.row)" type="text" size="small"
+          >查看</el-button
+        >
+        <el-button type="text" size="small">编辑</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    tableData: {
+      type: Array,
+      required: true,
+    },
+    titleData: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    handleClick(row) {
+      console.log(row);
+    },
+  },
+};
+</script>
